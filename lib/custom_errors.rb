dbd5a55089446e302adef5c8e49956cde097fd66
Kevin Nielsen <knielsen24@gmail.com>
2022-06-08 10:21:27 -0700

    commit tree 6f4a2eb4d9b63d95de69a4e551d05c71a460253f

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,40 @@
+require 'pry'
+
 class Person
-  attr_accessor :partner, :name
+	attr_accessor :partner, :name
 
-  def initialize(name)
-    @name = name
-  end
+	def initialize(name)
+	  @name = name
+	end
 
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
+	def get_married(person)
+		if person.is_a?(Person)
+		  self.partner = person
+		  person.partner = self
+		else
+		  begin
+			 raise PartnerError
+		  rescue PartnerError => error
+			 puts error.message
+		  end
+		end
+	 end
 
-end
+	class PartnerError < StandardError
+		def message
+			"you must give the get_married method an argument of an instance of the person class!"
+		end
+	end
+ end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
+
+def reload
+	load "lib/custom_errors.rb"
+end
 
 
 
